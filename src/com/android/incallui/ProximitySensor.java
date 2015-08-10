45f27d87074cc6c1bed788b32b1d691830beeb6e
STELIX <ssspinni@gmail.com>
2015-08-10 13:26:29 +0200

    Revert "Revert "Fix proximity sensor with proximity wake option""
    This reverts commit e8bd0a86c94b9a4aa25aa9c240d4db742231f50b.

--- src/com/android/incallui/ProximitySensor.java
+++ src/com/android/incallui/ProximitySensor.java
@@ -58,6 +58,7 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
     private int mOrientation = AccelerometerListener.ORIENTATION_UNKNOWN;
     private boolean mUiShowing = false;
     private boolean mIsPhoneOffhook = false;
+    private boolean hasOngoingCall = false;
     private boolean mIsPhoneOutgoing = false;
     private boolean mProximitySpeaker = false;
     private int mProxSpeakerDelay = 100;
@@ -127,7 +128,7 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
         // We ignore incoming state because we do not want to enable proximity
         // sensor during incoming call screen. We check hasLiveCall() because a disconnected call
         // can also put the in-call screen in the INCALL state.
-        boolean hasOngoingCall = InCallState.INCALL == newState && callList.hasLiveCall();
+        hasOngoingCall = InCallState.INCALL == newState && callList.hasLiveCall();
         boolean isOffhook = (InCallState.OUTGOING == newState) || hasOngoingCall;
         boolean isOutgoing = (InCallState.OUTGOING == newState);
 
@@ -261,13 +262,19 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
                     (mOrientation == AccelerometerListener.ORIENTATION_HORIZONTAL);
             screenOnImmediately |= !mUiShowing && horizontal;
 
+            // Turn screen on when call comes in and prevent accidental wake-up is off.
+            final boolean mProximityWakeEnabled =
+                Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.PROXIMITY_ON_WAKE, 0) == 1;
+            screenOnImmediately |= !mProximityWakeEnabled && mHasIncomingCall;
+
             // We do not keep the screen off when dialpad is visible, we are horizontal, and
             // the in-call screen is being shown.
             // At that moment we're pretty sure users want to use it, instead of letting the
             // proximity sensor turn off the screen by their hands.
             screenOnImmediately |= mDialpadVisible && horizontal;
 
-            Log.v(this, "screenonImmediately: ", screenOnImmediately);
+            Log.v(this, "screenOnImmediately: ", screenOnImmediately);
 
             Log.i(this, Objects.toStringHelper(this)
                     .add("keybrd", mIsHardKeyboardOpen ? 1 : 0)
@@ -276,6 +283,7 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
                     .add("hor", horizontal ? 1 : 0)
                     .add("ui", mUiShowing ? 1 : 0)
                     .add("aud", AudioState.audioRouteToString(audioMode))
+                    .add("proxywake", mProximityWakeEnabled ? 1 : 0)
                     .toString());
 
             if (mIsPhoneOffhook && !screenOnImmediately) {
